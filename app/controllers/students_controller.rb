dd0c063bee82a68e3be3f02fe38644c5e92c7a27
Juan Tristan <tristansatx210@gmail.com>
2018-12-25 06:25:27 +0000

    Extracted Redundancies

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,12 @@
 class StudentsController < ApplicationController
-  
+  before_action :set_student, only: [:show, :edit, :update]
   
   def index
     @students = Student.all
+  end
+  
+  def show
+    
   end
   
   def new
@@ -13,10 +17,26 @@ class StudentsController < ApplicationController
     @student = Student.new(student_params)
     if @student.save
       flash[:notice] = "Thank you for enrolling!"
-      redirect_to root_path
+      redirect_to @student
     else
       render 'new'
     end
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    if @student.update(student_params)
+      redirect_to @student
+    else
+      render 'edit'
+    end
+    
+  end
+  
+  def destroy
     
   end
   
@@ -26,4 +46,8 @@ class StudentsController < ApplicationController
     params.require(:student).permit(:name, :email)
   end
   
+  
+  def set_student
+    @student = Student.find(params[:id])
+  end
 end
