74b3eae35cfdd4ce076a905c2c2fcd4dc2cfef98
Juan Tristan <tristansatx210@gmail.com>
2019-01-08 02:19:49 +0000

    Create and Destroy Sessions, add Auth Methods

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  
+  def new
+    
+  end
+  
+  def create
+    student = Student.find_by(email: params[:sessions][:email].downcase)
+    if student && student.authenticate(params[:sessions][:password])
+      session[:student_id] = student.id
+      redirect_to student
+    else
+      flash.now[:notice] = "Incorrect Email/Password"
+      render 'new'
+    end
+  end
+  
+  def destroy
+    session[:student_id] = nil
+    redirect_to root_path
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   get 'courses/new', to: 'courses#new'
   get 'about', to: 'pages#about'
   resources :students, except: [:destroy]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
 end
