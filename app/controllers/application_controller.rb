6c9f2ba566d64ab8e53540029e32a1a2b5b55005
Juan Tristan <tristansatx210@gmail.com>
2019-01-08 22:07:40 +0000

    Restrict Actions and Views cleaned up layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
   helper_method :current_user, :logged_in?
+  
   def current_user
     @current_user ||= Student.find(session[:student_id]) if session[:student_id]
   end
   
   def logged_in?
     !!current_user
+  end
+  
+  def require_user
+    if !logged_in?
+      flash[:notice] = "Please log in!"
+      redirect_to login_path
+    end
   end
   
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,7 @@
 class StudentsController < ApplicationController
+  #skip_before_action :require_user, only: [:new, :create]
   before_action :set_student, only: [:show, :edit, :update]
+  before_action :require_same_student, only: [:edit, :update]
   
   def index
     @students = Student.all
@@ -50,4 +52,12 @@ class StudentsController < ApplicationController
   def set_student
     @student = Student.find(params[:id])
   end
+  
+  def require_same_student
+    if current_user != @student
+      redirect_to student_path(current_user)
+      flash[:notice] = "Profile Restricted"
+    end
+  end
+  
 end
